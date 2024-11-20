0e20b7d5f3da57bfe2cc85bbc2d48ba1631e2798
bogdanafilimonenko <bfilimonenko@gmail.com>
2024-11-20 21:28:25 +0200

    feature: desktop version, responsive to 'lg', part of burger menu for mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*.*"],
-    theme: {
-    extend: {}
+  theme: {
+    fontFamily: {
+      "black": "Poppins-Black, system-ui",
+      "medium": "Poppins-Medium, system-ui",
+      "regular": "Poppins-Regular, system-ui"
+    },
+    extend: {
+      colors: {
+        "accent": "#e6c744"
+      }
+    }
   }, plugins: []
 };
 
